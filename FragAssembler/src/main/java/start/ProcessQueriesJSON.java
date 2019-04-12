7f178e88c0ace78026c9cf929ca8fe786a8294c8
michaelwenk <michael.wenk@uni-jena.de>
2019-04-13 00:08:28 +0200

    -	generated structures are now ranked by Tanimoto coefficients 	regarding the query spectrum

--- FragAssembler/src/main/java/start/ProcessQueriesJSON.java
+++ FragAssembler/src/main/java/start/ProcessQueriesJSON.java
@@ -33,9 +33,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import match.Match;
 import model.SSC;
 import model.SSCLibrary;
 import org.openscience.cdk.exception.CDKException;
@@ -54,22 +58,27 @@ public class ProcessQueriesJSON {
     
     private final SSCLibrary sscLibrary;
     private final String pathToQueriesFile, pathToOutputsFolder;
-    private final int nThreads, nStarts;
+    private final int nThreads, nStarts, minMatchingSphere;
     private final SmilesParser smilesParser;
     private final BufferedReader br;
     private final TimeMeasurement tm;
+    private final double shiftTol, matchFactorThrs;
     
-    public ProcessQueriesJSON(final SSCLibrary sscLibrary, final String pathToQueriesFile, final String pathToOutputsFolder) throws FileNotFoundException {
-        this(sscLibrary, pathToQueriesFile, pathToOutputsFolder, 1, -1);
-    }
+//    public ProcessQueriesJSON(final SSCLibrary sscLibrary, final String pathToQueriesFile, final String pathToOutputsFolder) throws FileNotFoundException {
+//        this(sscLibrary, pathToQueriesFile, pathToOutputsFolder, 1, -1);
+//    }
 
-    public ProcessQueriesJSON(final SSCLibrary sscLibrary, final String pathToQueriesFile, final String pathToOutputsFolder, final int nThreads, final int nStarts) throws FileNotFoundException {
+    public ProcessQueriesJSON(final SSCLibrary sscLibrary, final String pathToQueriesFile, final String pathToOutputsFolder, final int nThreads, final int nStarts, final double shiftTol, final double matchFactorThrs, final int minMatchingSphere) throws FileNotFoundException {
         this.pathToQueriesFile = pathToQueriesFile;
         this.pathToOutputsFolder = pathToOutputsFolder;
         this.nThreads = nThreads;        
         this.sscLibrary = sscLibrary;
         this.sscLibrary.setNThreads(this.nThreads);
         this.nStarts = nStarts;
+        this.shiftTol = shiftTol;
+        this.matchFactorThrs = matchFactorThrs;
+        this.minMatchingSphere = minMatchingSphere;
+        
         this.smilesParser = new SmilesParser(SilentChemObjectBuilder.getInstance());
         this.br = new BufferedReader(new FileReader(this.pathToQueriesFile));
         this.tm = new TimeMeasurement();
@@ -82,9 +91,10 @@ public class ProcessQueriesJSON {
         SSCRanker sscRanker = new SSCRanker(this.sscLibrary, this.nThreads);
 
         IAtomContainer solutionAtomContainer;
-        ArrayList<Double> shiftsQuerySpectrum, shiftsSolutionSpectrum;
         int querySpectrumCounter = 0;
         BufferedWriter bw;
+        final HashMap<String, Float> tanimotoCoefficients = new HashMap<>();
+        ArrayList<Double> sortedQuerySpectrumShifts, sortedSolutionSpectrumShifts;
 
         System.out.println("\n\n-> processing query file: \"" + this.pathToQueriesFile + "\" ...");
 
@@ -92,24 +102,31 @@ public class ProcessQueriesJSON {
         String line;
         while (it.hasNext()) {
             line = it.next();
-            if(line.startsWith("#")){
+            if (line.startsWith("//")) {
+                continue;
+            }
+            if (line.startsWith("#")) {
                 System.out.println("\n\nskip query: " + querySpectrumCounter + " -> " + line + "\n");
                 querySpectrumCounter++;
                 continue;
             }
             System.out.println("\n\nnow processing query: " + querySpectrumCounter + " -> " + line + "\n");
 
-            querySpectrum = DB.NMRShiftDBSpectrumToSpectrum(line, Start.SPECTRUM_PROPERTY_ATOMTYPE);
+            querySpectrum = DB.NMRShiftDBSpectrumToSpectrum(line, "13C");
             System.out.println("\nquery spectrum:\t" + querySpectrum.getShifts(0)
                     + "\nequivalents:\t" + querySpectrum.getEquivalences()
-                    + "\nmultiplicities:\t" + querySpectrum.getMultiplicities() 
+                    + "\nmultiplicities:\t" + querySpectrum.getMultiplicities()
                     + "\nequivalent signals classes: " + querySpectrum.getEquivalentSignalClasses());
             
-            sscRanker.rank(querySpectrum, Start.PICK_PRECISION);
+            sscRanker.rank(querySpectrum, this.shiftTol);
             System.out.println("\n\nno. of matches: " + sscRanker.getMatchFactors().size());
             System.out.println("match factors: " + sscRanker.getMatchFactors());
-            System.out.println("ranked SSC indices: " + sscRanker.getRankedSSCIndices());
-            System.out.println("ranked match factors: " + sscRanker.getRankedMatchFactors());
+            System.out.println("ranked SSC indices: " + sscRanker.getRankedSSCLibrary().getSSCIndices());
+            final LinkedHashSet<Double> rankedMatchedFactors = new LinkedHashSet<>();
+            for (final long sscIndex : sscRanker.getRankedSSCLibrary().getSSCIndices()) {
+                rankedMatchedFactors.add(sscRanker.getMatchFactors().get(sscIndex));
+            }
+            System.out.println("ranked match factors: " + rankedMatchedFactors);
             System.out.println("ranked SSC library indices: " + sscRanker.getRankedSSCLibrary().getSSCIndices() + "\n");
 
             if ((this.nStarts > 0) && (this.nStarts < sscRanker.getMatchFactors().size())) {
@@ -119,30 +136,56 @@ public class ProcessQueriesJSON {
             }
             System.out.println("\nnumber of start SSCs for query " + querySpectrumCounter + ":\t" + nStartSSCs);
 
-            solutions = Assembly.assemble(nStartSSCs, this.nThreads, sscRanker.getRankedSSCLibrary(), Start.MIN_MATCHING_SPHERE_COUNT, querySpectrum, Start.SHIFT_TOL, Start.MATCH_FACTOR_THRS, Start.PICK_PRECISION);
+            solutions = Assembly.assemble(nStartSSCs, this.nThreads, sscRanker.getRankedSSCLibrary(), this.minMatchingSphere, querySpectrum, this.matchFactorThrs, this.shiftTol);
 
             System.out.println("\nsolutions for query " + querySpectrumCounter + " (" + line + "):\t" + solutions.size());
 
-            shiftsQuerySpectrum = querySpectrum.getShifts(0);
-            Collections.sort(shiftsQuerySpectrum);
+            final String[] solutionsSMILESToSort = new String[solutions.size()];
+            int solutionsCounter = 0;
+            for (final String smiles : solutions.keySet()) {
+                solutionsSMILESToSort[solutionsCounter] = smiles;
+                solutionsCounter++;
+            }
+            tanimotoCoefficients.clear();
+            for (final String smiles : solutionsSMILESToSort) {
+                try {
+                    tanimotoCoefficients.put(smiles, Match.calculateTanimotoCoefficient(querySpectrum, solutions.get(smiles).getSubspectrum(), 0));
+                } catch (CDKException ex) {
+                    tanimotoCoefficients.put(smiles, (float) 0.0);
+                }
+            }
+            
+            Arrays.parallelSort(solutionsSMILESToSort, new Comparator<String>() {
+                @Override
+                public int compare(final String smiles1, final String smiles2) {                    
+                    return -1 * Float.compare(tanimotoCoefficients.get(smiles1), tanimotoCoefficients.get(smiles2));
+                }
+            });
 
             bw = new BufferedWriter(new FileWriter(this.pathToOutputsFolder + "/results_" + querySpectrumCounter + ".smiles"));
-            for (final String smiles : solutions.keySet()) {
+            String smiles;
+            sortedQuerySpectrumShifts = querySpectrum.getShifts(0);
+            Collections.sort(sortedQuerySpectrumShifts);
+            for (int i = 0; i < solutionsSMILESToSort.length; i++) {
+                smiles = solutionsSMILESToSort[i];
                 solutionAtomContainer = this.smilesParser.parseSmiles(smiles);
+                sortedSolutionSpectrumShifts = solutions.get(smiles).getSubspectrum().getShifts(0);
+                Collections.sort(sortedSolutionSpectrumShifts);
 
-                shiftsSolutionSpectrum = solutions.get(smiles).getSubspectrum().getShifts(0);
-                Collections.sort(shiftsSolutionSpectrum);
-                System.out.println("query spectrum   :\t" + shiftsQuerySpectrum);
-                System.out.println("solution spectrum:\t" + shiftsSolutionSpectrum);
+                System.out.println("query spectrum   :\t" + sortedQuerySpectrumShifts);
+                System.out.println("solution spectrum:\t" + sortedSolutionSpectrumShifts);
                 System.out.println("SMILES:          :\t" + smiles);
                 System.out.println("              --> \t" + "atoms: " + solutionAtomContainer.getAtomCount() + ", bonds: " + solutionAtomContainer.getBondCount());
-
-                bw.append(smiles);
+                System.out.println("              --> \t" + "tanimoto: " + tanimotoCoefficients.get(smiles));
+                
+                
+                bw.append(smiles + " " + tanimotoCoefficients.get(smiles));
                 bw.newLine();
                 bw.flush();
             }
+            
             bw.close();
-            querySpectrumCounter++;
+            querySpectrumCounter++;            
         }
         this.br.close();
     }
