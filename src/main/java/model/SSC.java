8e19da4b5ce99b0399d14dbb0921aa087005a4df
michaelwenk <michael.wenk@uni-jena.de>
2019-09-30 21:37:38 +0200

    - updated the copyright

--- src/main/java/model/SSC.java
+++ src/main/java/model/SSC.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2019. Michael Wenk [https://github.com/michaelwenk]
+ * Copyright (c) 2019 Michael Wenk [https://github.com/michaelwenk]
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -105,8 +105,8 @@ public final class SSC {
      */
     public void update() throws CDKException {
         AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(this.substructure);
-        this.updateAtomTypeIndices();        
-        this.updateUnsaturatedAtomIndices();   
+        this.updateAtomTypeIndices();
+        this.updateUnsaturatedAtomIndices();
         this.updateHOSECodes();
         this.updateMultiplicitySections(); 
     }
