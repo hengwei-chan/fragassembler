15a3fb07292b58354af65d4bd2c531eee5b3b454
michaelwenk <michael.wenk@uni-jena.de>
2019-11-01 17:03:02 +0100

    - parallel SSC library creation enabled again - buildSSCs is now called buildSSCCollection

--- src/main/java/fragmentation/Fragmentation.java
+++ src/main/java/fragmentation/Fragmentation.java
@@ -21,10 +21,13 @@ import model.SSCLibrary;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import utils.ParallelTasks;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 public class Fragmentation {
@@ -44,34 +47,39 @@ public class Fragmentation {
      * @throws java.lang.InterruptedException
      *
      * @see casekit.NMR.dbservice.NMRShiftDB#getSSCComponentsFromNMRShiftDB(String, String)
-     * @see Fragmentation#buildSSCs(IAtomContainer, Spectrum, Assignment, int)
+     * @see Fragmentation#buildSSCCollection(IAtomContainer, Spectrum, Assignment, int)
      */
     public static SSCLibrary buildSSCLibrary(final HashMap<Integer, Object[]> SSCComponentsSet, final int maxSphere, final int nThreads) throws InterruptedException {
 
         final SSCLibrary sscLibrary = new SSCLibrary(nThreads);
-        for (final int index : SSCComponentsSet.keySet()){
-            try {
-                sscLibrary.extend(Fragmentation.buildSSCs((IAtomContainer) SSCComponentsSet.get(index)[0], (Spectrum) SSCComponentsSet.get(index)[1], (Assignment) SSCComponentsSet.get(index)[2], maxSphere));
-            } catch (CDKException e) {
-                e.printStackTrace();
-            }
+        final ConcurrentLinkedQueue<SSC> builtSSCs = new ConcurrentLinkedQueue<>();
+        final ArrayList<Callable<Collection<SSC>>> callables = new ArrayList<>();
+        // add all task to do
+        for (final int index: SSCComponentsSet.keySet()) {
+            callables.add(() -> Fragmentation.buildSSCCollection((IAtomContainer) SSCComponentsSet.get(index)[0], (Spectrum) SSCComponentsSet.get(index)[1], (Assignment) SSCComponentsSet.get(index)[2], maxSphere));
         }
+        ParallelTasks.processTasks(callables, sscCollection -> sscCollection.parallelStream().forEach(builtSSCs::add), nThreads);
+        sscLibrary.extend(builtSSCs);
 
-//        final ConcurrentLinkedQueue<SSC> builtSSCs = new ConcurrentLinkedQueue<>();
-//        final ArrayList<Callable<SSCLibrary>> callables = new ArrayList<>();
-//        // add all task to do
-//        for (final int index: SSCComponentsSet.keySet()) {
-//            callables.add(() -> Fragmentation.buildSSCs((IAtomContainer) SSCComponentsSet.get(index)[0], (Spectrum) SSCComponentsSet.get(index)[1], (Assignment) SSCComponentsSet.get(index)[2], maxSphere));
+//        HashMap<String, ArrayList<SSC>> map = new HashMap<>();
+//        for (final SSC ssc : builtSSCs){
+//            String extendedHOSECode = Compare.getExtendedHOSECode(ssc);
+//            if(!map.containsKey(extendedHOSECode)){
+//                map.put(extendedHOSECode, new ArrayList<>());
+//            }
+//            map.get(extendedHOSECode).add(ssc);
 //        }
-//        ParallelTasks.processTasks(callables, sscLibraryTemp -> builtSSCs.addAll(sscLibraryTemp.getSSCs()), nThreads);
-//        sscLibrary.extend(builtSSCs);
-
+//        long sscCount = 0;
+//        for(final Map.Entry<String, ArrayList<SSC>> entry : map.entrySet()){
+//            sscCount += entry.getValue().size();
+//        }
+//        System.out.println(" BUILT SSC COUNT -> " + builtSSCs.size() + " vs. MAP SIZE -> " + map.size() + ", " + sscCount);
 
         return sscLibrary;
     }
     
     /**
-     * Builds a set of substructure-subspectrum-correlations (SSC objects) from one
+     * Builds a collection of substructure-subspectrum-correlations (SSC objects) from one
      * structure for all its atoms by using a breadth first search 
      * with spherical limit. 
      *
@@ -84,7 +92,7 @@ public class Fragmentation {
      * @return
      * @see Fragmentation#buildSSC(IAtomContainer, Spectrum, Assignment, int, int)
      */
-    private static SSCLibrary buildSSCs(final IAtomContainer structure, final Spectrum spectrum, final Assignment assignment, final int maxSphere) throws CDKException {
+    public static Collection<SSC> buildSSCCollection(final IAtomContainer structure, final Spectrum spectrum, final Assignment assignment, final int maxSphere) throws CDKException {
         // if the structure contains explicit hydrogens atoms then, after 
         // removing them, the assignments have to be corrected 
         if(Utils.containsExplicitHydrogens(structure)){
@@ -102,19 +110,19 @@ public class Fragmentation {
                 }
             }
         }
-        final SSCLibrary sscLibrary = new SSCLibrary();        
+        final Collection<SSC> sscCollection = new ArrayList<>();
         SSC ssc;
         for (int i = 0; i < structure.getAtomCount(); i++) {      
             ssc = Fragmentation.buildSSC(structure, spectrum, assignment, i, maxSphere);
             // if one part of the structure could not be built then skip the whole structure
             // and return an empty SSC library
             if (ssc == null) {
-                return new SSCLibrary();
+                return new ArrayList<>();
             }
-            sscLibrary.insert(ssc);
+            sscCollection.add(ssc);
         }
-       
-        return sscLibrary;
+
+        return sscCollection;
     }        
     
     /**
@@ -136,7 +144,7 @@ public class Fragmentation {
      */
     public static SSC buildSSC(final IAtomContainer structure, final Spectrum spectrum, final Assignment assignment, final int rootAtomIndex, final int maxSphere) throws CDKException {
         Utils.setAromaticityAndKekulize(structure);
-        final ArrayList<Integer> substructureAtomIndices = new ArrayList<>(Fragmentation.buildSubstructureAtomIndicesSet(structure, rootAtomIndex, maxSphere));
+        final ArrayList<Integer> substructureAtomIndices = Fragmentation.buildSubstructureAtomIndicesList(structure, rootAtomIndex, maxSphere);
         final IAtomContainer substructure = Fragmentation.buildSubstructure(structure, rootAtomIndex, maxSphere);
         final Spectrum subspectrum = new Spectrum(spectrum.getNuclei());
         final Assignment subassignment = new Assignment(subspectrum);
@@ -173,11 +181,11 @@ public class Fragmentation {
      * @param maxSphere Spherical limit for building a substructure into 
      * all directions
      * @return
-     * @throws org.openscience.cdk.exception.CDKException
+     *
      * @see HOSECodeBuilder#buildConnectionTree(IAtomContainer, int, Integer)
      * @see HOSECodeBuilder#buildAtomContainer(ConnectionTree)
      */
-    public static IAtomContainer buildSubstructure(final IAtomContainer structure, final int rootAtomIndex, final int maxSphere) throws CDKException {
+    public static IAtomContainer buildSubstructure(final IAtomContainer structure, final int rootAtomIndex, final int maxSphere) {
         return HOSECodeBuilder.buildAtomContainer(HOSECodeBuilder.buildConnectionTree(structure, rootAtomIndex, maxSphere));
     }
     
@@ -191,10 +199,10 @@ public class Fragmentation {
      * @param maxSphere Spherical limit for building a substructure into 
      * all directions
      * @return
-     * @throws org.openscience.cdk.exception.CDKException
+     *
      * @see HOSECodeBuilder#buildConnectionTree(IAtomContainer, int, Integer)
      */
-    public static LinkedHashSet<Integer> buildSubstructureAtomIndicesSet(final IAtomContainer structure, final int rootAtomIndex, final int maxSphere) throws CDKException {
-        return HOSECodeBuilder.buildConnectionTree(structure, rootAtomIndex, maxSphere).getKeys(true);
+    public static ArrayList<Integer> buildSubstructureAtomIndicesList(final IAtomContainer structure, final int rootAtomIndex, final int maxSphere) {
+        return HOSECodeBuilder.buildConnectionTree(structure, rootAtomIndex, maxSphere).getKeys();
     }
 }
